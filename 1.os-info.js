23af5a39278e405864df471be4708e0a3ab06a0d
Yayo <yaelblack13@gmail.com>
2026-09-25 00:48:11 -0600

    feat: agregar modulos de os y fs, organizar paquetes

---
+++ 1.os-info.js
@@ -0,0 +1,9 @@
+﻿const os = require('node:os')
+
+console.log('Informacion del sistema operativo:')
+console.log('------------------------------')
+
+console.log('Nombre del sistema operativo', os.platform())
+console.log('Version del sistema operativo', os.release())
+console.log('Arquitectura', os.arch())
+console.log('CPUs', os.cpus())
--- index.js
+++
@@ -1,1 +0,0 @@
-﻿console.log('Servidor activo y escuchando');
